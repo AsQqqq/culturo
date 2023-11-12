19c49b536371afbd4dc7195acb01c0e19a2e489c
AsQqqq <danila.udodov.workmail@gmail.com>
2023-11-12 15:58:34 +0300

    Сейв

--- pages_backend/index/route_index.py
+++ pages_backend/index/route_index.py
@@ -1,9 +1,10 @@
-from flask import render_template, flash
+from flask import render_template, flash, jsonify, request
 from pages_backend import app
 from flask_login import current_user
 import main_index
 from database import get_data_database_testing
 from database.decorators import get_testing
+
 
 
 @app.route('/', methods=['GET'])
@@ -16,13 +17,43 @@ def index() -> render_template:
                 return render_template("main.html")
             else:
                 items = set(get_data_database_testing())
-                print(items)
                 return render_template(f"{main_index.index}.html", items=items)
         else:
             return render_template('index.html')
     except Exception as e:
         flash("Произошла внутренняя ошибка сервера. Обратитесь к администратору.", "error")
         return render_template('sign_in.html')
+
+
+
+
+@app.route('/get_data', methods=['GET', 'POST'])
+def get_data():
+    if request.method == "POST":
+        data_pages = request.get_json()
+        data = fetch_data_from_postgresql()
+        if data_pages == 'confirm':
+            result = "confirm_result"
+        elif data_pages == 'trash':
+            # Обработка данных для 'trash'
+            result = "trash_result"
+        else:   
+            # Обработка других случаев
+            result = "ERROR"
+        print(f"\n\n{result}\n\n")
+        return jsonify(data)
+    elif request.method == "GET":
+        data = fetch_data_from_postgresql()
+        return jsonify(data)
+
+def fetch_data_from_postgresql():
+    return ['123', '321', 'saddf', 'hfgjihgdf']
+
+
+
+
+
+
 
 def upload_index():
     return True
---
+++ static/script/mainPage.js
@@ -0,0 +1,115 @@
+var swipeElement = document.getElementById('swipe-container');
+var startX, startY;
+
+// Функция для выполнения запроса к серверу и обновления данных на странице
+function updateData() {
+    // Выполните AJAX-запрос к серверу для получения данных
+    fetch('/get_data')
+        .then(response => response.json())
+        .then(data => {
+            // Очистите текущие данные на странице
+            document.getElementById('data-list').innerHTML = '';
+
+            // Обновите данные на странице
+            data.forEach(record => {
+                const listItem = document.createElement('li');
+                listItem.textContent = record.field_name;  // Замените на соответствующее поле
+                document.getElementById('data-list').appendChild(listItem);
+            });
+        });
+}
+
+function sendConfirmResult() {
+    fetch('/get_data', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify('confirm'),
+    })
+}
+
+function sendTrashResult() {
+    fetch('/get_data', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify('trash'),
+    })
+}
+
+// Вызовите функцию обновления данных при загрузке страницы
+updateData();
+
+// Установите интервал для выполнения функции обновления каждую секунду
+setInterval(updateData, 10000);
+
+function darkenMainBlock() {
+    var imageContainer = document.getElementById("main-block");
+    imageContainer.classList.toggle("darken");
+}
+
+function unDarkenMainBlock() {
+    var imageContainer = document.getElementById("main-block");
+    imageContainer.classList.remove("darken");
+    swipeElement.addEventListener("touchend", unDarkenMainBlock);
+}
+
+// Проверяем, является ли устройство мобильным
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+if (isMobile) {
+    // Если мобильное устройство, используем touch события
+    swipeElement.addEventListener("touchstart", darkenMainBlock);
+    swipeElement.addEventListener("touchend", unDarkenMainBlock);
+} else {
+    // Если не мобильное устройство, используем mouse события
+    swipeElement.addEventListener('mousedown', function (e) {
+        darkenMainBlock();
+        // Запоминаем начальные координаты при нажатии
+        startX = e.clientX;
+        startY = e.clientY;
+        var leftElement = document.getElementById('trash-icon');
+        var rightElement = document.getElementById('confirm-icon');
+
+        // Добавляем/удаляем классы для запуска анимации
+        leftElement.classList.toggle('hidden');
+        leftElement.classList.toggle('trash-icon');
+
+        rightElement.classList.toggle('hidden');
+        rightElement.classList.toggle('confirm-icon');
+    });
+
+    swipeElement.addEventListener('mouseup', function (e) {
+        // Вычисляем разницу между начальными и конечными координатами
+        unDarkenMainBlock();
+        var leftElement = document.getElementById('trash-icon');
+        var rightElement = document.getElementById('confirm-icon');
+
+        // Добавляем/удаляем классы для запуска анимации
+        leftElement.classList.toggle('hidden');
+        leftElement.classList.toggle('trash-icon');
+
+        rightElement.classList.toggle('hidden');
+        rightElement.classList.toggle('confirm-icon');
+
+        var deltaX = e.clientX - startX;
+        var deltaY = e.clientY - startY;
+
+        // Проверяем, является ли свайп горизонтальным
+        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+            // Горизонтальный свайп
+            if (deltaX > 0) {
+                console.log('Свайп вправо');
+                sendConfirmResult();
+            } else {
+                console.log('Свайп влево');
+                sendTrashResult();
+            }
+        } else {
+            // Вертикальный свайп (здесь вы можете добавить необходимые действия или игнорировать)
+            console.log('Вертикальный свайп');
+        }
+    });
+}
